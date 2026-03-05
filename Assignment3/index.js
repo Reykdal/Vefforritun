7b37f35a3b50b7153640cf5bd739a582e33c5089
Reykdal <enok24@ru.is>
2026-03-05 14:07:19 +0000

    implemented part 4

--- Assignment3/index.js
+++ Assignment3/index.js
@@ -34,18 +34,58 @@ app.use((req, res, next) => {
 import { events, attendees } from "./data/initialData.js";
 import { nextEventId, nextAttendeeId } from "./data/initialData.js";
 
+const basePath = `${apiPath}${version}`; 
+
 /* --------------------------
 
       EVENTS ENDPOINTS     
 
 -------------------------- */
+app.get(`${basePath}/events`, (req, res) => {
+  let result = events;
+
+  const { name, location } = req.query;
+
+  if (name) {
+    const n = String(name).toLowerCase();
+    result = result.filter((e) => String(e.name).toLowerCase().includes(n))
+  }
+
+  if (location) {
+    const l = String(location).toLowerCase();
+    result = result.filter((e) => String(e.name).toLowerCase().includes(l))
+  }
+
+  return res.status(200).json(result)
+});
+
+app.get(`${basePath}/events/:eventId`, (req, res) => {
+  const id = Number(req.params.eventId);
+
+  if (!Number.isInteger(id)) {
+    return res.status(400).json({ message: "id must be a number." });
+  }
+
+  const event = events.find((e) => e.id === id);
+
+  if (!event) {
+    return res.status(404).json({ message: "event not found" });
+  }
+
+  const attendeeCount = attendees.filter((a) => Array.isArray(a.eventIds) && a.eventIds.includes(id)).length;
+
+  return res.status(200).json({ ...event, attendeeCount });
+});
+
 
 /* --------------------------
 
     ATTENDEES ENDPOINTS    
 
 -------------------------- */
-
+app.get(`${basePath}/attendees`, (req, res) => {
+  return res.status(200).json(attendees);
+});
 /* --------------------------
 
       SERVER INITIALIZATION  
